7644e984f9b4a3b253fef512e5925dd63cd9cabb
phdfam <phdfam@protonmail.com>
2024-12-26 09:32:17 +0200

    Bug fix

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 source .env
 
+set -x
 python3 manage.py makemigrations solax_registers || exit 1
 python3 manage.py migrate || exit 1
 REST_API_ADDRESS=${1:-"localhost"}:${2:-"8000"} gunicorn -c gunicorn_conf.py
